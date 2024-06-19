72d178e0f263d8f30f5845d5536ec37d9340367d
UARR7 <capt4001@gmail.com>
2024-06-19 19:15:42 +0530

    commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div  className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quasi eligendi blanditiis, accusantium repellat porro atque molestiae ab esse nulla hic. Illo rerum quos beatae nisi consectetur voluptatum, magni sed.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
